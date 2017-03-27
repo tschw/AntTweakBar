02764b1f323a60e2befc12b7d49193a8391e2f55
tschw <tschwinger@isonews2.com>
2017-03-27 05:16:03 +0200

    Make DX shaders compile without VisualStudio

--- scripts/D3DShader.cmake
+++ scripts/D3DShader.cmake
@@ -12,9 +12,11 @@ function(add_hlsl_header input shader type dependentSources)
 			ABSOLUTE ${CMAKE_CURRENT_SOURCE_DIR})
 
 	set(output "${name}_${shader}.h")
+	get_filename_component(absOutput ${output}
+			ABSOLUTE ${CMAKE_CURRENT_BINARY_DIR})
 
 	add_custom_command(
-		OUTPUT ${output}
+		OUTPUT ${absOutput}
 		COMMAND
 			${ATB_D3D11_FXC}
 			/T ${type} /E ${shader}
@@ -22,7 +24,7 @@ function(add_hlsl_header input shader type dependentSources)
 		MAIN_DEPENDENCY ${absInput})
 
 	set_property(SOURCE ${dependentSources} ${ARGN}
-			APPEND PROPERTY OBJECT_DEPENDS ${output})
+			APPEND PROPERTY OBJECT_DEPENDS ${absOutput})
 
 endfunction()
 
